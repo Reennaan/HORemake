42795967c56bd8717b7b155789cb84ef9a7a0734
Renan <qwerty142999@gmail.com>
2026-04-02 16:52:54 -0300

    updating-mango-again

--- App.tsx
+++ App.tsx
@@ -243,7 +243,7 @@ const App: React.FC = () => {
         <DesktopIcon
           name="Mango"
           iconUrl={import.meta.env.BASE_URL+"img/icon.jpg"}
-          onDoubleClick={() => openExternal("https://github.com/Reennaan/Mango2/releases/tag/v1.0.2")}
+          onDoubleClick={() => openExternal("https://github.com/Reennaan/Mango./releases/tag/1.0.0")}
           selected={selectedIcon === "Mango"}
           onSelect={() => setSelectedIcon("Mango")}
         />
